ee4c6ee847613f996876a3a40a6790213e007b16
John Kusner <jkusner321@gmail.com>
2016-07-21 09:00:19 -0400

    Started on validation testing

---
+++ Test/Validation/dotnet/ValidationTest/ValidationTest/Verify.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Verify.aspx.cs" Inherits="ValidationTest.Verify" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <%= CheckSig() %>
+</body>
+</html>
---
+++ Test/Validation/dotnet/ValidationTest/ValidationTest/Verify.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ValidationTest
+{
+    public partial class Verify : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected string CheckSig()
+        {
+            var formData = Request.Form;
+            var text = formData["txtSign"];
+            var sig = formData["txtSig"];
+
+            string output = "INVALID!";
+
+            if (!string.IsNullOrEmpty(sig))
+            {
+                try
+                {
+                    ContentInfo contentInfo = new ContentInfo(Encoding.UTF8.GetBytes(text));
+
+                    SignedCms signedCms = new SignedCms(contentInfo, true);
+
+                    signedCms.Decode(Convert.FromBase64String(sig));
+
+                    // This checks if the signature is valid, but doensn't actually verify the cert (TODO)
+                    signedCms.CheckSignature(true);
+
+                    output = "Signature valid.";
+
+                    signedCms.CheckSignature(false);
+
+                    output += "<br>Cert valid";
+                }
+                catch (Exception e)
+                {
+                    output += "<br>" + e.ToString();
+                }
+            }
+
+            return output;
+        }
+    }
+}
