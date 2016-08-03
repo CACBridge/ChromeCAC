4a733311aba6b644785b9db45cc2ba9d2c541278
John Kusner <jkusner321@gmail.com>
2016-08-03 09:33:02 -0400

    Boilerplating for Installer

---
+++ Installer/Windows_.NET_Installer/Windows_.NET_Installer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_.NET_Installer
+{
+    class Program
+    {
+        const string ExtensionID = "";
+        const string RegistryPath = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beginning install");
+        }
+
+        static void SetupExtension()
+        {
+
+        }
+
+        static void SetupNativeApp()
+        {
+
+        }
+
+        static void SetupRegistry()
+        {
+
+        }
+    }
+}
