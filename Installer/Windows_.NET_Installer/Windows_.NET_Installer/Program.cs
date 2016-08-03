2b444bd94f5cd134e5016e45219a1f4785d5242a
Connor Roth <crothdev@gmail.com>
2016-08-03 15:25:57 -0400

    setupExtension method done, needs if else though

--- Installer/Windows_.NET_Installer/Windows_.NET_Installer/Program.cs
+++ Installer/Windows_.NET_Installer/Windows_.NET_Installer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,24 +12,40 @@ namespace Windows_.NET_Installer
     {
         const string ExtensionID = "";
         const string RegistryPath = "";
+        static string NativeAppPath = "";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Beginning install");
+            SetupExtension();
         }
 
         static void SetupExtension()
         {
+            //download from github
+            WebClient Client = new WebClient();
+            string tempFile = Path.GetTempFileName();
+            Client.DownloadFile("https://cdn.rawgit.com/CACBridge/ChromeCAC/master/ChromeExtensionReleases/v0.1/ChromeExtension.crx", tempFile);
+
+            Console.WriteLine("{0}",tempFile);
+            //put it folder
+            string newDir = @"C:\\User\\ChromeCAC\\extensionInstaller";
+            Directory.Move(tempFile, newDir);
 
         }
 
         static void SetupNativeApp()
         {
+            //intsall chromcac.net into appdata, registry points to this
+            
+            
+            //edit manifest to contain path to exe, needs to be used for registry
 
         }
 
         static void SetupRegistry()
         {
+            // look at resitry.reg
 
         }
     }
